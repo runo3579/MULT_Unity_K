327c5b310fd0093a5ac255b69c26e24a3a8413b0
runo3579 <143289946+runo3579@users.noreply.github.com>
2023-11-03 13:13:10 -0700

    Hour14

---
+++ kche_Hour14and16/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "kche_Hour14";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
